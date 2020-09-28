0f05d3005495d07c5e5024cec8a977c5cea06588
kazuya002ex <hara.ex002@gmail.com>
2020-09-28 12:21:33 +0900

    Add horror

--- js/main.js
+++ js/main.js
@@ -2,17 +2,37 @@
 
 {
 
+    const kanae = document.getElementById("kanae");
+    const videoo = document.getElementById("video");
+    const nru = document.getElementById("nru");
+
+    function redirect(){
+        location.href="result.html";
+    }
+
+    function ho(){
+        videoo.style.display = "none";
+        nru.style.display = "none";
+        kanae.style.display = "block";
+
+        setTimeout(redirect, 5000);
+    }
+
+    function alertMsgd(){
+        alert("5秒経過しました");
+    }
 
     // play を押したときの処理
     const play = document.getElementById('play');
 
-    var video = $('#video01').get(0); 
+    var video = $('#video').get(0);
 
     play.addEventListener('click', () => {
+        //動画を再生
         $('#play').remove();
         video.play();
-        // タイマーを起動
 
-        //動画を再生
+        // 19秒後にかなえを表示&効果音を鳴らす
+        setTimeout(ho, 19000);
     });
 }
